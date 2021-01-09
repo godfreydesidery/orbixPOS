ec9fd347ca870a2f43ef36e8afa5f63d29bd9934
godfreydesidery <desideryg@gmail.com>
2021-01-09 21:48:29 +0300

    added service templates

---
+++ src/app/cart.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  constructor() { }
+
+  createCart(){
+
+  }
+  syncronizeCart(){
+
+  }
+  loadItemToCart(){
+
+  }
+  editCartItem(){
+
+  }
+  voidCartItem(){
+
+  }
+  unvoidCartitem(){
+
+  }
+  holdCart(){
+
+  }
+  unholdCart(){
+
+  }
+  emptyCart(){
+
+  }
+  destroyCart(){
+    
+  }
+
+
+}
+export class Cart{
+  id : any
+  till : {}
+}
+export class CartDetail{
+  id : any
+  cart : {}
+}
